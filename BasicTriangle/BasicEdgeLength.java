47fd9f34ae28750c11385b7c7ffc4ab69d4e596e
maloneyg <pax_vobiscum@hotmail.com>
2013-08-01 22:55:33 +0200

    BasicEdgeLength compiles, but gives an error at runtime. There's some problem with a dependency in the google guava library.

--- BasicTriangle/BasicEdgeLength.java
+++ BasicTriangle/BasicEdgeLength.java
@@ -14,6 +14,13 @@ final public class BasicEdgeLength implements AbstractEdgeLength<BasicPoint> {
     static final private ImmutableSet<Initializer.EDGE_LENGTH> LENGTHS = Initializer.LENGTHS;
 
     /*
+    * A list of all possible BasicEdgeLength objects.
+    * When someone asks for a BasicEdgeLength, we just give 
+    * his one of these.
+    */
+    static final private ImmutableList<BasicEdgeLength> ALL_EDGE_LENGTHS;
+
+    /*
     * A list of vector representatives of the allowable edge lengths.
     * Each one starts at the origin and lies on the positive x-axis.
     */
@@ -26,20 +33,12 @@ final public class BasicEdgeLength implements AbstractEdgeLength<BasicPoint> {
         for (int i = 2; i < preReps.length; i++)
             preReps[i] = preReps[i-1].inflate().subtract(preReps[i-2]);
         REPS = ImmutableList.copyOf(preReps);
-    }
 
-    // private constructor
-    private BasicEdgeLength(int i) {
-        UnmodifiableIterator<Initializer.EDGE_LENGTH> itr = LENGTHS.iterator();
-        for (int j = 0; j < i+1; j++) {
-            if (itr.hasNext()) {
-                if (j == i)
-                    length = itr.next();
-                else
-                    itr.next();
-            }
-        }
-        rep = REPS.get(i);
+        // initialize ALL_EDGE_LENGTHS.
+        BasicEdgeLength[] preAllEdgeLengths = new BasicEdgeLength[LENGTHS.size()];
+        for (int j = 0; j < LENGTHS.size(); j++)
+            preAllEdgeLengths[j] = new BasicEdgeLength(j);
+        ALL_EDGE_LENGTHS = ImmutableList.copyOf(preAllEdgeLengths);
     }
 
     /*
@@ -54,6 +53,29 @@ final public class BasicEdgeLength implements AbstractEdgeLength<BasicPoint> {
     */
     final private BasicPoint rep;
 
+    // private constructor
+    private BasicEdgeLength(int i) {
+        UnmodifiableIterator<Initializer.EDGE_LENGTH> itr = LENGTHS.iterator();
+        Initializer.EDGE_LENGTH tempLength = itr.next();
+        for (int j = 0; j < i; j++) {
+            if (itr.hasNext()) {
+                if (j == i)
+                    tempLength = itr.next();
+                else
+                    itr.next();
+            }
+        }
+        rep = REPS.get(i);
+        length = tempLength;
+    }
+
+    // public static factory method
+    static public BasicEdgeLength createBasicEdgeLength(int i) {
+        if (i < 0 || i > ALL_EDGE_LENGTHS.size()-1)
+            throw new IllegalArgumentException("Incorrect edge length index.");
+        return ALL_EDGE_LENGTHS.get(i);
+    }
+
     public boolean equals(Object obj) {
         if (obj == null || getClass() != obj.getClass())
             return false;
@@ -61,8 +83,27 @@ final public class BasicEdgeLength implements AbstractEdgeLength<BasicPoint> {
         return this.length.equals(l.length);
     }
 
+    public int hashCode() {
+        return length.hashCode();
+    }
+
+    public String toString() {
+        return "Edge length " + rep;
+    }
+
     public BasicPoint getAsVector() {
         return rep;
+    }
+
+    public static void main(String[] args) {
+
+        BasicEdgeLength EL0 = createBasicEdgeLength(0);
+        BasicEdgeLength EL1 = createBasicEdgeLength(1);
+        BasicEdgeLength EL2 = createBasicEdgeLength(2);
+        System.out.println(EL0);
+        System.out.println(EL1);
+        System.out.println(EL2);
+
     }
 
 } // end of class BasicEdgeLength
