f4d0abddeed63021f664d44c57582d642def33e7
maloneyg <pax_vobiscum@hotmail.com>
2013-11-12 22:26:09 +0000

    Something is wrong, but I don't know what. It compiles, but doesn't run, and reports an error in the static initialization of BasicEdgeLength.

--- BasicTriangle/EdgeBreakdownGraph.java
+++ BasicTriangle/EdgeBreakdownGraph.java
@@ -18,6 +18,8 @@ import java.io.Serializable;
 import org.jgrapht.graph.*;
 import org.jgrapht.graph.SimpleGraph;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 // an unordered pair of edge lengths.
 class DoubleEdgeLength implements Serializable {
@@ -72,7 +74,7 @@ public class EdgeBreakdownGraph implements Serializable {
 
     private SimpleGraph<List<Integer>,DoubleEdgeLength> G = new SimpleGraph(DoubleEdgeLength.class);
     private ImmutableList<BasicEdgeLength> ALL_EDGE_LENGTHS = BasicEdgeLength.ALL_EDGE_LENGTHS;
-    private List<List<Integer>>[] sorted = (List<List<Integer>>[]) new ArrayList[ALL_EDGE_LENGTHS.size()];
+    private Set<List<Integer>>[] sorted = (Set<List<Integer>>[]) new HashSet[ALL_EDGE_LENGTHS.size()];
 
     // private constructor
     private EdgeBreakdownGraph() {
@@ -84,13 +86,35 @@ public class EdgeBreakdownGraph implements Serializable {
     }
 
     // add some edge breakdowns
-    public void add(List<Integer> b0, List<Integer> b1, List<Integer> b2, BasicEdgeLength l0, BasicEdgeLength l1, BasicEdgeLength l2) {
+    public void add(ImmutableList<Integer> b0, ImmutableList<Integer> b1, ImmutableList<Integer> b2, BasicEdgeLength l0, BasicEdgeLength l1, BasicEdgeLength l2) {
         sorted[ALL_EDGE_LENGTHS.indexOf(l0)].add(b0);
         sorted[ALL_EDGE_LENGTHS.indexOf(l1)].add(b1);
         sorted[ALL_EDGE_LENGTHS.indexOf(l2)].add(b2);
         G.addVertex(b0);
         G.addVertex(b1);
         G.addVertex(b2);
+        List<Integer> c0 = b0.reverse();
+        List<Integer> c1 = b1.reverse();
+        List<Integer> c2 = b2.reverse();
+        sorted[ALL_EDGE_LENGTHS.indexOf(l0)].add(c0);
+        sorted[ALL_EDGE_LENGTHS.indexOf(l1)].add(c1);
+        sorted[ALL_EDGE_LENGTHS.indexOf(l2)].add(c2);
+        G.addVertex(c0);
+        G.addVertex(c1);
+        G.addVertex(c2);
+        // now add all the bonds
+        G.addEdge(b0,b1,DoubleEdgeLength.createDoubleEdgeLength(l0,l1));
+        G.addEdge(b1,b2,DoubleEdgeLength.createDoubleEdgeLength(l1,l2));
+        G.addEdge(b2,b0,DoubleEdgeLength.createDoubleEdgeLength(l2,l0));
+        G.addEdge(c0,c1,DoubleEdgeLength.createDoubleEdgeLength(l0,l1));
+        G.addEdge(c1,c2,DoubleEdgeLength.createDoubleEdgeLength(l1,l2));
+        G.addEdge(c2,c0,DoubleEdgeLength.createDoubleEdgeLength(l2,l0));
+        G.addEdge(b0,c1,DoubleEdgeLength.createDoubleEdgeLength(l0,l1));
+        G.addEdge(b1,c2,DoubleEdgeLength.createDoubleEdgeLength(l1,l2));
+        G.addEdge(b2,c0,DoubleEdgeLength.createDoubleEdgeLength(l2,l0));
+        G.addEdge(c0,b1,DoubleEdgeLength.createDoubleEdgeLength(l0,l1));
+        G.addEdge(c1,b2,DoubleEdgeLength.createDoubleEdgeLength(l1,l2));
+        G.addEdge(c2,b0,DoubleEdgeLength.createDoubleEdgeLength(l2,l0));
     }
 
     // test client
