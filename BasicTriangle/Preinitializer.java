e9fb11ce3b42d63adac70ca223dd066d021b654b
maloneyg <pax_vobiscum@hotmail.com>
2013-11-14 16:58:48 +0000

    This will search tile 1 on factor a+b+c.

--- BasicTriangle/Preinitializer.java
+++ BasicTriangle/Preinitializer.java
@@ -14,7 +14,7 @@ class Preinitializer {
 
     public static final int N = 11;             // the order of symmetry
 
-    public static final int MY_TILE = 3;        // the tile we're searching
+    public static final int MY_TILE = 4;        // the tile we're searching
 
     public static final float EP = 0.000001f;  // threshold value
 
@@ -27,17 +27,20 @@ class Preinitializer {
     // the inflation factor, represented as coefficients of
     // 1, a, a^2, etc., where a = 2*cos(pi/N).
 
-    //public static final ImmutableList<Integer> INFL = ImmutableList.of(1, 1); // really small search. Won't work at all for tile 3.
-    //public static final ImmutableList<Integer> INFL = ImmutableList.of(1, 2, 1); // the square of the really small search
-    //public static final ImmutableList<Integer> INFL = ImmutableList.of(-1, 0, 1); // small search
-    //public static final ImmutableList<Integer> INFL = ImmutableList.of(-1, 1, 1); // big search
-    public static final ImmutableList<Integer> INFL = ImmutableList.of(1, -1, 0, 1); // quite big search (110)
+    //public static final ImmutableList<Integer> INFL = ImmutableList.of(1, 1); // really small search. Won't work at all for tile 3. (1+a)
+    //public static final ImmutableList<Integer> INFL = ImmutableList.of(1, 2, 1); // the square of the really small search (1+a)^2 won't work with tile 3 
+    //public static final ImmutableList<Integer> INFL = ImmutableList.of(-1, 0, 1); // small search (104)
+    //public static final ImmutableList<Integer> INFL = ImmutableList.of(-1, 1, 1); // big search (105) 
+    //public static final ImmutableList<Integer> INFL = ImmutableList.of(1, -1, 0, 1); // quite big search (110)
     //public static final ImmutableList<Integer> INFL = ImmutableList.of(-1, -2, 1, 1); // quite big search (111)
+    //public static final ImmutableList<Integer> INFL = ImmutableList.of(2, 0, -3, 0, 1); // huge search (115)
     //public static final ImmutableList<Integer> INFL = ImmutableList.of(1, 1, -3, 0, 1); // huge search (116)
     //public static final ImmutableList<Integer> INFL = ImmutableList.of(2, 1, -3, 0, 1); // superhuge search (117)
     //public static final ImmutableList<Integer> INFL = ImmutableList.of(1, -2, -3, 1, 1); // even huger search (121)
     //public static final ImmutableList<Integer> INFL = ImmutableList.of(2, -2, -3, 1, 1); // even huger search (122)
     //public static final ImmutableList<Integer> INFL = ImmutableList.of(0, -2, -2, 1, 1); // even huger search (124)
+    //public static final ImmutableList<Integer> INFL = ImmutableList.of(0, 1, 1); // 1 + a + b (106)
+    public static final ImmutableList<Integer> INFL = ImmutableList.of(-1, -1, 1, 1); // a + b + c 
 
     public static final ImmutableList<ImmutableList<Integer>> PROTOTILES = ImmutableList.of( 
                              //    ImmutableList.of( 1, 2, 4 ),  // seven
